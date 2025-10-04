37ce2f92ba0d3b9b307cd4d4a53934301e94d53d
ffluffy <blackxdiego@gmail.com>
2025-10-04 11:40:55 -0600

    created pygame window, created class to manage game assets and behavior, created main game loop, made a game instance, and ran the game

---
+++ alien_invasion.py
@@ -0,0 +1,36 @@
+import sys
+
+import pygame
+
+class AlienInvasion:
+    """Overall class to manage game assets and behavior."""
+    
+    def __init__(self):
+        """Initialize the game, and create game resources."""
+        # pygame.init() function initialized the background settings that Pygame needs to work properly
+        # called pygame.display.set_mode to create a display window on which we'll draw all the game's graphical elements
+        # set the display window to the attribue 'self.screen', so it will be available in all methods in the class
+        pygame.init()
+        
+        self.screen = pygame.display.set_mode((1200, 800))
+        pygame.display.set_caption("Alien Invasion")
+
+    def run_game(self):
+        """Start the main loop for the game."""
+        # the event 'pygame.QUIT' is the x button
+        while True:
+            #Watch for keyboard and mouse events.
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+            # flip() is the display to put your work on screen(makes the most recently drawn screen visible).
+            pygame.display.flip()
+# checks wheter the file is being run directly.Also prevents some code from running when importing this file as a module
+#  (so basically you need to call the function directly for it to run)
+if __name__ == '__main__':
+    # Make a game instance, and run the game.
+    ai = AlienInvasion()
+    ai.run_game()
+
+
