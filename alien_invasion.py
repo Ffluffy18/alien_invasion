c9bed0b8ed76838a67e46446d49298ee13dff077
ffluffy <blackxdiego@gmail.com>
2025-10-05 13:19:13 -0600

    refactoring: _check_events() method to keep track of user input, _update_screen() method to update images on screen and flip to the new screen.

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,6 +4,8 @@ import pygame
 
 from settings import Settings
 
+from ship import Ship
+
 class AlienInvasion:
     """Overall class to manage game assets and behavior."""
     
@@ -20,20 +22,31 @@ class AlienInvasion:
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
 
+        self.ship = Ship(self)
+
     def run_game(self):
         """Start the main loop for the game."""
-        # the event 'pygame.QUIT' is the x button
         while True:
-            #Watch for keyboard and mouse events.
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-
-            #redraw the screen during eacch pass through the loop.
-            self.screen.fill(self.settings.bg_color)
-            # flip() is the display to put your work on screen(makes the most recently drawn screen visible).
-            pygame.display.flip()
+            # calls the methods _check_events() into the loop
+            # to call this methods from within the class, I used dot notation with the variable self and name of the methods
+            self._check_events()
+            self._update_screen()
             self.clock.tick(60)
+
+    def _check_events(self):
+        """Respond to keypresses and mouse events."""
+        # the event 'pygame.QUIT' is the x at the top right of screen
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+    def _update_screen(self):
+        """Update images on the screen, and flip to the new screen."""
+        #redraw the screen during eacch pass through the loop.
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+        # flip() is the display to put your work on screen(makes the most recently drawn screen visible).
+        pygame.display.flip()
+
 # checks wheter the file is being run directly.Also prevents some code from running when importing this file as a module
 #  (so basically you need to call the function directly for it to run)
 if __name__ == '__main__':
